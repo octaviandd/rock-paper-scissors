d07d4ab358657cdba093ac786a749acf1dee8081
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2020-01-11 22:21:57 +0000

    new structure

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-
-let userScore = 0;
-let computerScore = 0;
-let ties = 0;
-let rounds = 0;
 const userScore_span = document.getElementById('user_score');
 const computerScore_span = document.getElementById('computer_score');
 const tiesNumber = document.getElementById('ties_number');
@@ -11,8 +6,15 @@ const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
 const roundNumber = document.getElementById('roundnumber');
 const resetScore = document.getElementById('reset_button')
+const winner = document.querySelector('#winner')
 
 
+
+
+let userScore = 0;
+let computerScore = 0;
+let ties = 0;
+let rounds = 0;
 
 function computerRandomPick(){
   const choices = ['rock', 'paper', 'scissors'];
@@ -21,37 +23,41 @@ function computerRandomPick(){
 
 }
 
-function win(){
-  userScore++
-  userScore_span.innerHTML = userScore
-}
+const scoreBoard = (function(){
+  function win(){
+    userScore++
+    userScore_span.innerHTML = userScore
+  }
+  
+  function lose() {
+    computerScore++
+    computerScore_span.innerHTML = computerScore
+  }
+  
+  function draw() {
+    ties++
+    ties_number.innerHTML = ties
+  }
+  
+  function addRound(){
+    rounds ++
+    round_number.innerHTML = 'Round: ' + rounds
+  }
+  
+  function reset(){
+    rounds = 0;
+    userScore = 0;
+    computerScore = 0;
+    ties = 0;
+    round_number.innerHTML = 'Round: '
+    ties_number.innerHTML = '0'
+    computerScore_span.innerHTML = '0'
+    userScore_span.innerHTML = '0'
+  }
 
+  return {win, lose, draw, addRound, reset}
 
-function lose() {
-  computerScore++
-  computerScore_span.innerHTML = computerScore
-}
-
-function draw() {
-  ties++
-  ties_number.innerHTML = ties
-}
-
-function addRound(){
-  rounds ++
-  round_number.innerHTML = 'Round: ' + rounds
-}
-
-function reset(){
-  rounds = 0;
-  userScore = 0;
-  computerScore = 0;
-  ties = 0;
-  round_number.innerHTML = 'Round: '
-  ties_number.innerHTML = '0'
-  computerScore_span.innerHTML = '0'
-  userScore_span.innerHTML = '0'
-}
+})()
 
 
 function game(userChoice){
@@ -60,29 +66,27 @@ function game(userChoice){
     case "paperrock":
     case "rockscissors":
     case "scissorspaper":
-      alert('Computer picked: ' + computerChoice + ', you won.')
-      win()
-      addRound()
+      winner.innerHTML = 'Computer picked: ' + computerChoice + ', you won.'
+      scoreBoard.win()
+      scoreBoard.addRound()
       break
     case "rockpaper":
     case "scissorsrock":
     case "paperscissors":
-      alert('Computer picked: ' + computerChoice + ', you lost.')
-      lose()
-      addRound()
+      winner.innerHTML = 'Computer picked: ' + computerChoice + ', you lost.'
+      scoreBoard.lose()
+      scoreBoard.addRound()
       break
     case 'rockrrock':
     case 'paperpaper':
     case 'scissorsscissors':
-      alert("You both picked " + computerChoice + " it's a draw.")
-      draw()
-      addRound()
+      winner.innerHTML = "You both picked " + computerChoice + ", it's a draw."
+      scoreBoard.draw()
+      scoreBoard.addRound()
       break
   }
-
-
-
 }
+
 
 
 function main(){
@@ -98,7 +102,7 @@ function main(){
       game('scissors')
     })
     resetScore.addEventListener('click', function () {
-      reset()
+      scoreBoard.reset()
     })
 }
 
